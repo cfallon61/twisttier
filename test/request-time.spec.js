42e48a01711711248eb0cea5abc4931137479954
cfallon61 <fallon2@purdue.edu>
2019-09-13 21:51:59 -0400

    removed assert.fail

--- test/request-time.spec.js
+++ test/request-time.spec.js
@@ -1,8 +1,8 @@
 //test/request-time.spec.js
 const assert = require('assert');
 
-describe('Test Assert Fail', function() {
-    it('should fail immediately', function() {
-        assert.fail("Testing if the build will fail and not deploy in pipeline");
-    });
-});
+// describe('Test Assert Fail', function() {
+//     it('should fail immediately', function() {
+//         assert.fail("Testing if the build will fail and not deploy in pip");
+//     });
+// });

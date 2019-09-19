5639fdc9830ba633ef7c2b2d931e320a03f8a8cd
Kira <cs307team8@outlook.com>
2019-09-18 20:14:54 -0400

    conflict reslolved

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ class App extends Component {
             To get started, edit <code>src/App.js</code> and save to reload. helllooooo
           <Spin className="spintest" username="tgul" content="Say hi to spins." timestamp="12:56 pm, 9/18/2019" />
           </p>
-          <Button label="this is fun" name="b1" text="heelo" variant="primary" onClick={() => { alert('alert'); }}> button 1 </Button>
         </Router>
 
 

a4289c775c55e9fe77864cde62684c3ea0c1c56c
nailt <nailtarcan@gmail.com>
2019-10-01 19:25:32 -0400

    Seperated home view to logged in or not logged in.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button';
 import Spin from './spin.jsx';
 
-const Home = () => {
-    return (
-        <div>
-            <Spin className="spintest" username="tgul" content="Say hi to spins." timestamp="12:56 pm, 9/18/2019" />
-        </div>
-    )
+class Home extends Component{
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            loggedIn : false
+        };  
+    }
+
+    render()
+    {
+        if(this.state.loggedIn)
+        {
+            //User timeline view
+        }
+        else
+        {
+            //Default view
+            return (
+                <div>
+                    <Spin className="spintest" username="tgul" content="Say hi to spins." timestamp="12:56 pm, 9/18/2019" />
+                </div>
+            );
+        }
+    }
 }
 
 export default Home

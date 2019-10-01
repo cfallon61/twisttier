96b2b79be88fb2fb17f46d80aa2b99d6c022075a
Sid-V <sidv@purdue.edu>
2019-10-01 00:29:49 -0400

    setup user settings page

--- src/components/UserSettings.js
+++ src/components/UserSettings.js
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import profilePic from './profilepicIcon.png';
 import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 
 class UserSettings extends Component {
@@ -16,7 +20,24 @@ class UserSettings extends Component {
       username: "",
       email:"",
       bio:"",
-    }
+    };
+  
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleEditBio = this.handleEditBio.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+
+  handleChangeEmail(event){
+
+  }  
+
+  handleChangePassword(event){
+
+  }
+
+  handleEditBio(event){
+
   }
 
   getUserInfo(){
@@ -52,12 +73,37 @@ class UserSettings extends Component {
     
     return (
       <div>
-        <h1> username </h1>
-        <Image src={profilePic} />
+        <Container>
+          <Row>
+            <Col><h1> username </h1></Col>
+          </Row>
+          <Row>
+           <Col><Image src={profilePic} /></Col>
+          </Row>
+        
+          <Row>
+            <Col xs={10}><h2> email </h2></Col>
+            <Col>
+              <Button variant="secondary" onClick={this.handleChangeEmail}>Change Email</Button>
+            </Col>
+          </Row>
+          
+          <Row>
+            <Col xs={10}><h3> Password </h3></Col>
+            <Col>
+              <Button variant="secondary" onClick={this.handleEditBio}>Change password</Button>
+            </Col>
+          </Row>
 
-        <h2>email</h2>
+          <Row>
+            <Col xs={10}><h3> bio </h3></Col>
+            <Col>
+              <Button variant="secondary" onClick={this.handleEditBio}>Edit bio</Button>
+            </Col>
+          </Row>
 
-        <h3> bio</h3>
+        </Container>
+
       </div>
     );
   }
